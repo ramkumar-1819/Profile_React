5e2c6477ad867ff874c84f8df7526db00941aebf
ramkumar-1819 <rohitram1819@gmail.com>
2021-03-21 09:19:11 +0530

    Final Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Make(props){          //this component makes a section for each leader and return it
+    return(<div className="details">
+        <img src={props.image} alt="leader images"></img>
+        <div>Name:<b>{props.name}</b></div>
+        <div>Birth:<b>{props.birth}</b></div>
+        <div>State:<b>{props.state}</b></div>
+        </div>)
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Leaders(){     //this component contains leader details and return when all section for leaders are maked.
+    var Details=[{image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Ambedkar_1966_stamp_of_India_%28cropped%29.jpg/150px-Ambedkar_1966_stamp_of_India_%28cropped%29.jpg",name:"B.R.Ambedkar",birth:"(1891–1956)",state:"Maharastra"},
+                 {image:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/APJ_Abdul_Kalam_2015_stamp_of_India_%28cropped%29.jpg/150px-APJ_Abdul_Kalam_2015_stamp_of_India_%28cropped%29.jpg",name:"A.P.J.Abdul Kalam",birth:"(1931–2015)",state:"TamilNadu"},
+                 {image:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Vallabhbhai_Patel_1975_stamp_of_India.jpg/150px-Vallabhbhai_Patel_1975_stamp_of_India.jpg",name:"Vallabhbhai Patel",birth:"(1875–1950)",state:"Gujarat"},
+                 {image:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Jawaharlal_Nehru_stamp_%28cropped%29.jpg/150px-Jawaharlal_Nehru_stamp_%28cropped%29.jpg",name:"Jawaharlal Nehru",birth:"(1889–1964)",state:"Uttar Pradesh"},
+                 {image:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Mutter_Teresa_von_Kalkutta.jpg/150px-Mutter_Teresa_von_Kalkutta.jpg",name:"Mother Teresa",birth:"(1910–1997)",state:"West Bengal"}
+                ]
+    return( 
+        <div className="section">{
+        Details.map(val=>{
+        return<Make image={val.image} name={val.name} birth={val.birth} state={val.state}/>
+    })  }
+        </div>
+    )
+}
+
+ReactDOM.render(<Leaders/>,document.getElementById("root"))
---
+++ src/index1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Details1 extends React.Component{
+    render(){
+        return(<div>Name:hi{this.props.name}</div>)
+    }
+}
+
+export default Details1
